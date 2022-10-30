95438a3cbc53945d33864e54cc49e730a3f7493c
Shepard <Crazyhistorian2000@yandex.ru>
2022-10-31 01:50:53 +0300

    settings front

---
+++ client/src/pages/CosmeticsPages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CosmeticsPages = () => {
+    return (
+        <div>
+            CosmeticsPages
+        </div>
+    );
+};
+
+export default CosmeticsPages;
---
+++ client/src/routes.js
@@ -0,0 +1,20 @@
+import Admin from "./pages/Admin";
+import {ADMIN_ROUTE, BASKET_ROUTE} from "./utils/consts";
+
+export const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
+        Component: Admin
+    },
+    {
+        path: BASKET_ROUTE,
+        Component: Basket
+    },
+]
+
+export const publicRoutes = [
+    {
+
+    }
+
+]
